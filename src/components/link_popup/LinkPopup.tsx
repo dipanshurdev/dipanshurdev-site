f1ab280a269345ac87e51dc89a98349a6b8b1c35
Aman <dv451197@gmail.com>
2024-09-13 15:16:08 +0530

    patch: screenshot popup

--- src/components/link_popup/LinkPopup.tsx
+++ src/components/link_popup/LinkPopup.tsx
@@ -14,14 +14,14 @@ const LinkPopup = (props: Props) => {
       <div className="hover-container">
         <a
           href={url}
-          className="underline-animation"
+          className="underline-animation  "
           target="_blank"
           rel="noreferer"
         >
           <div className="hover-image">
             <img
               src={`/images/${image}`}
-              className="h-[150px] w-full border-4 border-neutral-900 rounded-lg"
+              className="h-[150px] w-full border-4 border-neutral-800 rounded-lg"
               alt="Hover Image"
             />
           </div>
