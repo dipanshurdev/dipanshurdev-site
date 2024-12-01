948166da5df9eca085e5341741d74e88bd7075ee
Dipanshu Rawat <dv451197@gmail.com>
2024-12-01 23:42:45 +0530

    add: my unique username🤤

--- src/components/Hero/HeroDesc.tsx
+++ src/components/Hero/HeroDesc.tsx
@@ -16,7 +16,7 @@ const HeroDesc = () => {
   return (
     <div className="w-full text-dark dark:text-light">
       <span> Hey visitor 👋, I'm </span>
-      <MyName name="Dipanshu Rawat" />
+      <MyName name="dipanshurdev" />
       <div className="text-3xl leading-snug ">
         <span style={{ marginRight: `${widthPx}px` }}></span> a Fullstack
         Developer. I mostly like doing Frontend with
