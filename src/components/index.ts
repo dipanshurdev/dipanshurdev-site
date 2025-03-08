abeafbd8316fa9d8b5603d3ec6ba6a019544d242
Dipanshu Rawat <dv451197@gmail.com>
2025-03-09 00:11:22 +0530

    chore: migrate Navbar.astro to React as Navbar.tsx

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
-import Navbar from "./Navbar.astro";
 import Footer from "./Footer.astro";
 import Heading from "./Heading.astro";
 import Project from "./Project.astro";
 
-export { Navbar, Footer, Heading, Project };
+export { Footer, Heading, Project };
