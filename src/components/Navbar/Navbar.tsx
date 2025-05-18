0bb12b5278efc5f6ee32fe745fe6e9ef80690e34
Dipanshu Rawat <dv451197@gmail.com>
2025-05-18 18:12:06 +0530

    fix(style): underline overflow and icon

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -14,8 +14,8 @@ const Navbar = () => {
         </span>
       </div>
 
-      <div className="flex items-center gap-4">
-        <div className="text-blue-700 hidden md:flex items-center gap-1">
+      <div className="flex items-center gap-6">
+        <div className="  text-blue-700 hidden md:flex items-center gap-1">
           <a href="/">home</a>
           <svg
             width="24"
@@ -32,7 +32,7 @@ const Navbar = () => {
           </svg>
         </div>
 
-        <div className="underline hidden md:flex items-center gap-1">
+        <div className=" underline hidden md:flex items-center gap-1">
           <a href="/projects">projects</a>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -46,10 +46,26 @@ const Navbar = () => {
             ></path>
           </svg>
         </div>
-
-        <span className="underline hidden md:inline-block">
-          <a href="/about-me">About me</a>
-        </span>
+        <div className="  underline hidden md:flex items-center gap-1">
+          <a href="/about-me">about</a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="lucide lucide-smile-icon lucide-smile"
+          >
+            <circle cx="12" cy="12" r="10" />
+            <path d="M8 14s1.5 2 4 2 4-2 4-2" />
+            <line x1="9" x2="9.01" y1="9" y2="9" />
+            <line x1="15" x2="15.01" y1="9" y2="9" />
+          </svg>
+        </div>
 
         <span className="underline hidden md:inline-block">
           <ResumeLink />
